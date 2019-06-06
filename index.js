364d8f9a09ca340adbd26d17c6679d8603cbe864
StevenDixonDev <stevedixon1128.dev@gmail.com>
2019-06-06 16:26:50 -0400

    working on splitting file correctly

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ function loadDataFromFile(file, callback){
 }
 
 function test(err, data){
-  console.log(data);
+  //console.log(err)
+  //console.log(data)
+  let chunks = splitIntoChunks(data);
+  //let editedChunks = chunks.map((item, index)=> console.log(item, `index:${index}`)); 
+  //console.log(chunks.length)
+  
 }
-//function 
+//function
+
+function splitIntoChunks(data){
+  //split the data file into chunks based on the fact that all mbox messages start with a single line break followed by From
+  let chunkArray = data.match(/From(.*\n+)*?-{2} (.*\n)+?\n/gm)
+  console.log(chunkArray)
+  //return data.split(/\n(?=From[^:])/gm);
+}
+
+function replaceTextInBody(textItem){
+  return textItem.replace(/(?<=(From) (.*\n)+)\n(.*\n*)+(?=\n--)/gm, reverseBody)
+}
+
+function reverseBody(regexMatch){
+  return regexMatch.split(/\n/gm).reverse().join('\r');
+}
+
+loadDataFromFile('./mbox.full', test);
